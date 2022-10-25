d8d623335ce0da4447778adf96983dda21c48d59
Jiazhen Yang <JYang56@slb.com>
2022-10-25 11:11:34 +0800

    feat: add evennumbergreaterthen10

--- src/Foo.ts
+++ src/Foo.ts
@@ -1,5 +1,22 @@
 export class Foo {
   bar() {
     return 'bar'
-  }
+  }}
+
+export function isCuteNumberGreaterThan10(num:number[]):boolean {
+  const evenList = findAllEvenNumber(num)
+  const maxNum = findTheMax(evenList)
+  return isGreaterthan10(maxNum)
+}
+
+function findTheMax(num:number[]):number{
+  return Math.max(...num)
+}
+
+function findAllEvenNumber(num:number[]):number[]{
+  return num.filter(num=>num%2==0)
+}
+
+function isGreaterthan10(num:number):boolean{
+  return num > 10
 }
